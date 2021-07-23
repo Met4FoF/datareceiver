23dfc48df767ab8a37304e0cba426af8b25e8e21
Unknown <bene.see@gmail.com>
2021-07-23 09:22:59 +0000

    GPS reference Data added

--- data/GPS_GPIO_Input_calculator.py
+++ data/GPS_GPIO_Input_calculator.py
@@ -2,7 +2,7 @@ import h5py as h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
-hdf=h5py.File('data/multiBordGPSLog2.hfd5', "r+")
+hdf=h5py.File('20210721MultiBoard_GPS_test.hfd5', "r+")
 maxpoints=hdf['RAWDATA/0x60ad1e00_STM32_GPIO_Input'].attrs['Data_point_number']
 times1=hdf['RAWDATA/0x60ad1e00_STM32_GPIO_Input/Absolutetime'][0,11:maxpoints]
 times1uncer=hdf['RAWDATA/0x60ad1e00_STM32_GPIO_Input/Absolutetime_uncertainty'][0,11:maxpoints]
