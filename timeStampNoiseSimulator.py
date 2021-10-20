64dea5a3ab19c8e4c7d1423837617f33a3c3e51a
Benedikt Seeger <bene.see@gmail.com>
2021-10-20 12:26:16 +0200

    improved noise calculator

--- timeStampNoiseSimulator.py
+++ timeStampNoiseSimulator.py
@@ -4,6 +4,11 @@ import sinetools.SineTools as st
 from multiprocessing import Pool
 from tqdm.contrib.concurrent import process_map
 from matplotlib import cm
+from scipy.optimize import curve_fit
+
+def gaus(x,a,sigma):
+    x0=0
+    return a*np.exp(-(x-x0)**2/(2*sigma**2))
 
 plt.rc('font', family='serif')
 plt.rc('text', usetex=True)
@@ -18,7 +23,7 @@ plt.rc("axes", titlesize=SMALL_SIZE)  # fontsize of the axes title
 plt.rc("axes", labelsize=MEDIUM_SIZE)  # fontsize of the x and y labels
 plt.rc("xtick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
 plt.rc("ytick", labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
-plt.rc("legend", fontsize=SMALL_SIZE)  # legend fontsize
+plt.rc("legend", fontsize=SMALL_SIZE/1.5)  # legend fontsize
 plt.rc("figure", titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
 def generateFitWithPhaseNoise(freq,fs=1000,t_jitter=100e-9,lengthInS=0.1):
@@ -41,13 +46,13 @@ def getmuAndSTdForFreq(testparams,numOfruns=100):
 if __name__ == "__main__":
 
     freqPoints=200
-    ampPoints=6
-    nsPreAmpStep=20
+    ampPoints=12
+    nsPreAmpStep=10
     lengthInS=10
     freqs=np.zeros(freqPoints*ampPoints)
     noiseLevel=np.zeros(freqPoints*ampPoints)
     for i in range(ampPoints):
-        tmpFreqs=np.logspace(1.00,3.30,freqPoints)
+        tmpFreqs=np.logspace(1.00,3.3,freqPoints)
         freqToNear=(tmpFreqs % 1000) < 5
         freqToNear+=(tmpFreqs % 500) < 5
         freqToAdd=10*freqToNear
@@ -68,8 +73,8 @@ if __name__ == "__main__":
         ax[1].plot(freqs[i * freqPoints: (i + 1) * freqPoints],
                    results[i * freqPoints: (i + 1) * freqPoints, 1] / np.pi * 180,
                    label=r"\textbf{jitter= " + str(i * nsPreAmpStep) + " ns}")
-    ax[0].legend()
-    ax[1].legend()
+    ax[0].legend(ncol=4)
+    ax[1].legend(ncol=4)
     ax[1].set_xlabel(r"\textbf{Frequenz in Hz}")
     ax[0].set_ylabel(r"$2\sigma \varphi$ \textbf{in} $^\circ$")
     ax[1].set_ylabel(r"$\overline{\varphi}$ \textbf{in} $^\circ$")
@@ -77,21 +82,32 @@ if __name__ == "__main__":
     ax[1].grid(True)
 
     fig.show()
-
+    bw=np.ones(ampPoints)
     fig1,ax=plt.subplots(2)
     fig1.suptitle(r"\textbf{SampleRate = 1 kHz Dauer = "+str(lengthInS)+' s}')
     for i in range(ampPoints):
-        ax[0].plot(freqs[i * freqPoints: (i + 1) * freqPoints],
+        tmpFreqs=freqs[i * freqPoints: (i + 1) * freqPoints]
+        ax[0].plot(tmpFreqs,
                    200*results[i * freqPoints: (i + 1) * freqPoints,2],
-                   label=r"\textbf{jitter= "+str(i*nsPreAmpStep)+" ns}")
-        ax[1].plot(freqs[i * freqPoints: (i + 1) * freqPoints],
-                   results[i * freqPoints: (i + 1) * freqPoints, 3],
-                   label=r"\textbf{jitter= " + str(i * nsPreAmpStep) + " ns}")
+                   label=r"\textbf{Jitter = "+str(i*nsPreAmpStep)+" ns}")
+        AMPS=results[i * freqPoints: (i + 1) * freqPoints, 3]
+        dataPlot=ax[1].plot(tmpFreqs,
+                   AMPS,
+                   label=r"\textbf{Jitter = " + str(i * nsPreAmpStep) + " ns}")
+        if i!=0:
+            popt, pcov = curve_fit(gaus, tmpFreqs, AMPS, p0=[1, 5e5])
+            ax[1].plot(tmpFreqs,gaus(tmpFreqs,popt[0],popt[1]),label=r"\textbf{ Fit Bandbreite = }"+"{:.2f}".format(abs(popt[1])/1e6)+" MHz",color=dataPlot[-1].get_color(),ls='--')
+            bw[i]=popt[1]
+            print('______'+str(i * nsPreAmpStep)+' ns ___________')
+            print(popt)
+            print(popt[1]/(i * nsPreAmpStep*10e-9)*(i * nsPreAmpStep*10e-9))
+            print('_____________________________________________')
     ax[0].legend()
-    ax[1].legend()
+    ax[0].legend(ncol=4)
+    ax[1].legend(ncol=4)
     ax[1].set_xlabel(r"\textbf{Frequenz in Hz}")
     ax[0].set_ylabel(r"$2\sigma(\hat{A})$ \textbf{in \%}")
-    ax[1].set_ylabel(r"$\overline{\hat{A}}$ \textbf{in A.U.}")
+    ax[1].set_ylabel(r"$\overline{\hat{A}}$ \textbf{in R.U.}")
     ax[0].grid(True)
     ax[1].grid(True)
 
@@ -110,8 +126,9 @@ if __name__ == "__main__":
     freqs=np.zeros(freqPoints*lengthPoints)
     noiseLevel=np.zeros(freqPoints*lengthPoints)
     length = np.ones(freqs.size)
+
     for i in range(lengthPoints):
-        tmpFreqs=np.logspace(1.00,3.3,freqPoints)
+        tmpFreqs=np.logspace(1.00,6.0,freqPoints)
         freqToNear=(tmpFreqs % 1000) < 5
         freqToNear = (tmpFreqs % 1000) > 995
         freqToNear+=(tmpFreqs % 500) < 5
@@ -134,8 +151,8 @@ if __name__ == "__main__":
         ax[1].plot(freqs[i * freqPoints: (i + 1) * freqPoints],
                    results[i * freqPoints: (i + 1) * freqPoints, 1] / np.pi * 180,
                    label=r"\textbf{Dauer = " + "{:.2f}".format(StartLength/((i+1)*(i+1))) + " s}")
-    ax[0].legend(ncol=2)
-    ax[1].legend(ncol=2)
+    ax[0].legend(ncol=4)
+    ax[1].legend(ncol=4)
     ax[1].set_xlabel(r"\textbf{Frequenz in Hz}")
     ax[0].set_ylabel(r"$2\sigma(\varphi)$ \textbf{in} $^\circ$")
     ax[1].set_ylabel(r"$\overline{\varphi}$ \textbf{in} $^\circ$")
